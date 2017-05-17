c95d93825c4dd85555e757093a857dbc8adb32fb
Marc A Hester <marc@radsectors.com>
2017-05-17 11:48:15 -0500

    WIP for format feature

--- src/legacy.php
+++ src/legacy.php
@@ -4,14 +4,14 @@ namespace {
     if (!function_exists('pq')) {
         function pq()
         {
-            call_user_func_array(['radsectors\urp', 'pq'], func_get_args());
+            call_user_func(['radsectors\urp', 'pq'], func_get_args());
         }
     }
 
     if (!function_exists('pqd')) {
         function pqd()
         {
-            call_user_func_array(['radsectors\urp', 'pqd'], func_get_args());
+            call_user_func(['radsectors\urp', 'pqd'], func_get_args());
             die();
         }
     }
