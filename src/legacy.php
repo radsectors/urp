e293ad28fd0f4db148704a115ba2c811f2aa8c27
Marc A Hester <marc@radsectors.com>
2017-07-25 12:24:09 -0500

    fixes?

--- src/legacy.php
+++ src/legacy.php
@@ -2,16 +2,15 @@
 namespace {
 
     if (!function_exists('pq')) {
-        function pq()
-        {
-            call_user_func(['radsectors\urp', 'pq'], func_get_args());
+        function pq() {
+            call_user_func_array(['radsectors\urp', 'pq'], func_get_args());
         }
     }
 
     if (!function_exists('pqd')) {
         function pqd()
         {
-            call_user_func(['radsectors\urp', 'pqd'], func_get_args());
+            call_user_func_array(['radsectors\urp', 'pqd'], func_get_args());
             die();
         }
     }
